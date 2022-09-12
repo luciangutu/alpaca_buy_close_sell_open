b22bbe740302ecad9c503372fc58b9d9e9864317
Lucian <lgutu@amberstudio.com>
2022-09-12 16:06:58 +0300

    added the actual logic

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
 from dateutil.tz import tzlocal
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import dateutil.relativedelta
 import config
 import logging
 
 # trading setup
-symbol = 'SPY'
-paper = True  # paper trading
-trading_client = TradingClient(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY, paper=paper)
+SYMBOL = 'SPY'
+PAPER = True  # paper trading
+TIME_BUFFER = 900  # time buffer to trade after market is open and before market is close, in seconds
+trading_client = TradingClient(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY, paper=PAPER)
 data_client = StockHistoricalDataClient(config.APCA_API_KEY_ID, config.APCA_API_SECRET_KEY)
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -78,7 +79,7 @@ def sell_open(_symbol):
 
 def get_symbol_last_price(_symbol):
     """
-    :return: the last 15-minute delayed price for symbol
+    :return: the last 15-minute delayed price for SYMBOL
     """
     try:
         latest_quote = data_client.get_stock_latest_quote(StockLatestQuoteRequest(symbol_or_symbols=[_symbol]))
@@ -88,25 +89,34 @@ def get_symbol_last_price(_symbol):
         return None
 
 
-logging.info(get_account().status)
-
-# if market not open, exit
-if not is_market_open().is_open:
+def main():
+    logging.info(get_account().status)
     open_at = is_market_open().next_open.astimezone(tzlocal())
+    close_at = is_market_open().next_close.astimezone(tzlocal())
     time_now = datetime.now(tzlocal())
-    remaining_time = dateutil.relativedelta.relativedelta(time_now, open_at)
-    logging.info(f"Market not open! {remaining_time}")
-    exit()
 
-if list_positions():
-    logging.info(list_positions())
-else:
-    logging.info("No opened positions!")
+    # if market not open, exit
+    if not is_market_open().is_open:
+        remaining_time = dateutil.relativedelta.relativedelta(time_now, open_at)
+        logging.info(f"Market not open! {remaining_time}")
+        exit()
+    else:
+        if list_positions():
+            logging.info(list_positions())
+        else:
+            logging.info("No opened positions!")
 
-# ToDo: validate if SELL is happening near market open time and if BUY is happening near market close time
-# if it's close to market open time, then SELL (if we have open positions)
-# If it's close to market close time, then BUY (only if we don't have any opened positions)
-if get_symbol_last_price(symbol):
-    logging.info(buy_close(symbol))
-else:
-    logging.warning("Cannot get the last 15-minute delayed price for symbol!")
+        # validate if SELL is happening near market open time and if BUY is happening near market close time
+        if get_symbol_last_price(SYMBOL):
+            # if it's close to market close time, then BUY (only if we don't have any opened positions)
+            if round((close_at-time_now).total_seconds()) < TIME_BUFFER and not list_positions():
+                logging.info(buy_close(SYMBOL))
+            # if it's close to market open time, then SELL (if we have open positions)
+            if round((time_now-open_at).total_seconds()) < TIME_BUFFER:
+                logging.info(sell_open(SYMBOL))
+        else:
+            logging.warning(f"Cannot get the last 15-minute delayed price for {SYMBOL}!")
+
+
+if __name__ == "__main__":
+    main()
